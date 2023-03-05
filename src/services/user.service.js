898a65b6e8438f6ae5ca7986aa4570ad7479ed04
Wellington Lorenço <wlsouza@protonmail.com>
2023-03-05 19:58:03 -0300

    add validation of id format on userService

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,14 @@
 import { UserModel } from "../models/User.js";
+import mongoose from "mongoose";
 
 export async function create(body){
     return await UserModel.create(body);
 };
 
 export async function getById(id){
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return;
+    };
     return await UserModel.findById(id);
 }
 
